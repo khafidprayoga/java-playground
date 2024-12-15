d9a911e56aeef0ee558b29837f770be91f91fce8
Khafid Prayoga <khafidp@pm.me>
2024-12-15 12:42:39 +0700

    add example of usage with extended interface contract
    purpose: to avoid incopatible interface contract on public used contract, so we need to extend that interface rather than modify to add new method directly

--- src/main/java/id/my/khafidprayoga/Main.java
+++ src/main/java/id/my/khafidprayoga/Main.java
@@ -4,6 +4,14 @@ interface Payable {
     public boolean send(int address, long values) throws PaymentError;
 }
 
+interface USDC extends Payable {
+    public void mint(int address, long values) throws PaymentError;
+
+    public void burn(int address, long values) throws PaymentError;
+
+    public void bridge(int address, int toL2Address, long values) throws PaymentError;
+}
+
 class SolanaWallet implements Payable {
     @Override
     public boolean send(int address, long values) throws PaymentError {
@@ -37,13 +45,49 @@ class EthereumWallet implements Payable {
     }
 }
 
+class OptimismWallet extends EthereumWallet implements USDC {
+
+    @Override
+    public void mint(int address, long values) throws PaymentError {
+        System.out.printf("mint amount %d USDC on the adddress %s%n", values, address);
+    }
+
+    @Override
+    public void burn(int address, long values) throws PaymentError {
+        System.out.printf("burning amount %d on the adddress %s to blackhole%n", values, address);
+    }
+
+    @Override
+    public void bridge(int address, int toL2Address, long values) throws PaymentError {
+        System.out.println("Init bridge provider Stargate.... ");
+        System.out.println("Selecting best route");
+    }
+
+    @Override
+    public boolean send(int address, long values) throws PaymentError {
+        var msg = String.format("Transferring amount of %d USDC to address %s sucessfully", values, address);
+        System.out.println(msg);
+        return true;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         int usernameAccount = 100011231;
         Payable payer = new EthereumWallet();
+
         // change dependency
         payer = new SolanaWallet();
 
+        // init usdc of extended payable contract
+        OptimismWallet optimism = new OptimismWallet();
+
+        // change to v2 using usdc on optimism L2 solution
+        payer = optimism;
         payer.send(usernameAccount, 20);
+
+        // mint add token to the address
+        USDC op = optimism;
+        op.mint(1001, 200);
     }
 }
