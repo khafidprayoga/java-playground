5f29ce73b48b300810a17449d730d23d93074609
Khafid Prayoga <khafidp@pm.me>
2024-12-18 16:05:13 +0700

    add Consumer example lambdas util

--- src/main/java/id/my/khafidprayoga/Main.java
+++ src/main/java/id/my/khafidprayoga/Main.java
@@ -1,18 +1,37 @@
 package id.my.khafidprayoga;
 
+import java.util.List;
 import java.util.Random;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.IntSupplier;
-import java.util.function.Supplier;
 
 public class Main {
 
     public static void main(String[] args) {
+        // Supplier produce return
         Random rand = new Random(50L);
         // this code more performant than Supplier<Integer>
         IntSupplier newRandom = () -> rand.nextInt(100);
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 2; i++) {
             System.out.println(newRandom.getAsInt());
         }
+
+        // Consumer with param args but without return
+        Consumer<String> invoiceConsumer = data -> {
+            System.out.println("Producing invoice for: ".concat(data));
+        };
+
+        List<String > invoiceScheduled = List.of("Joe Arifin","Johan Dalton");
+        invoiceScheduled.forEach(invoiceConsumer);
+
+        BiConsumer<Random, Integer> randomNumberPrint = (random, integer) -> {
+            for (int i = 0; i < integer; i++) {
+                System.out.println("next rand=" + random.nextInt());
+            }
+        };
+
+        randomNumberPrint.accept(new Random(314L), 2);
     }
 }
