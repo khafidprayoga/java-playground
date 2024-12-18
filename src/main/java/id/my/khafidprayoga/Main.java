7197207141c11fbf9823122a7a8ba717627bea1b
Khafid Prayoga <khafidp@pm.me>
2024-12-18 15:30:02 +0700

    example lambda

--- src/main/java/id/my/khafidprayoga/Main.java
+++ src/main/java/id/my/khafidprayoga/Main.java
@@ -1,17 +1,44 @@
 package id.my.khafidprayoga;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
+
+@FunctionalInterface
+interface Runnable {
+    public abstract void run();
+}
+
+@FunctionalInterface
+interface Consumer<T> {
+    void accept(T t);
+
+    default Consumer<T> andThen(Consumer<? super T> after) {
+        return null;
+    }
+
+    ;
+}
+
+public class Main {
+    static int calculateTotalPrice(List<?> products){
+        AtomicInteger total = new AtomicInteger(0);
+        Consumer<?> consumer = product -> total.addAndGet(100);
+        return total.get();
+    }
+    public static void main(String[] args) {
+        Consumer<String> consume = s -> System.out.println(s);
+
+        consume.accept("Hello");
+
+        Runnable r = () -> System.out.println("I am running");
+
+        r.run();
+
+        Predicate<String> prec = s -> s.length() > 0;
+        System.out.println(prec.test("Hello world"));
+        var x = Main.calculateTotalPrice(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        System.out.println(x);
     }
 }
