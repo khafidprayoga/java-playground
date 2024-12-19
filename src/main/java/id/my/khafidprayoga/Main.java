93b795cc97020f59ed633508b6e80b863830682f
Khafid Prayoga <khafidp@pm.me>
2024-12-19 15:52:02 +0700

    working with collections and arrays

--- src/main/java/id/my/khafidprayoga/Main.java
+++ src/main/java/id/my/khafidprayoga/Main.java
@@ -1,17 +1,51 @@
 package id.my.khafidprayoga;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Collection<String> strings = new ArrayList<String>();
+
+        strings.add("one");
+        strings.add("two");
+        strings.add("three");
+
+        boolean isContainsTwo = strings.contains("two");
+
+        Collection<String > search = new ArrayList<>();
+        search.add("one");
+        search.add("two");
+
+        boolean isExist = strings.containsAll(search);
+
+        strings.addAll(search);
+
+        Collection<String> tobeRemove = new ArrayList<>();
+        tobeRemove.add("three");
+        strings.removeAll(tobeRemove);
+
+        Collection<String> retained = new ArrayList<>();
+        retained.add("one");
+        strings.retainAll(retained);
+        System.out.println("asd");
+
+
+        Collection<Integer> absentNum = new ArrayList<>();
+        System.out.println(absentNum.isEmpty());
+        System.out.println(strings.size());
+
+
+        String[] tabStr = strings.toArray(new String[strings.size()]);
+
+        Collection<String> words = Arrays.asList("hello","world","iam","here");
+
+        String[] tab = words.toArray(String[]::new);
+
+
+
     }
 }
